6626972c511e15b441619b80f5d4e41af540000a
mmontano08 <83566868+mmontano08@users.noreply.github.com>
2021-07-11 21:38:36 -0500

    Add files via upload

--- UFOs/static/js/data.js
+++ UFOs/static/js/data.js
@@ -999,33 +999,3 @@ var data = [
     comments: "Bright orange object&#44 rapid speed and direction changes .."
   }
 ];
-
-data.forEach((dataRow) => {
-  let row = tbody.append("tr");
-  Object.values(dataRow).forEach((val) => {
-    let cell = row.append("td");
-    cell.text(val);
-    }
-  );
-});
-
-function buildTable(data) {
-  // First, clear out any existing data
-  tbody.html("");
-
-  // Next, loop through each object in the data
-  // and append a row and cells for each value in the row
-  data.forEach((dataRow) => {
-    // Append a row to the table body
-    let row = tbody.append("tr");
-
-    // Loop through each field in the dataRow and add
-    // each value as a table cell (td)
-    Object.values(dataRow).forEach((val) => {
-      let cell = row.append("td");
-      cell.text(val);
-      }
-    );
-  });
-}
-
